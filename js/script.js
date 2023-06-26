700de2d011dfa1a755aeb11463c5431e2096af91
Soujanya <Soujanya262002@gmail.com>
2023-06-26 19:43:45 +0530

    my first project-tie

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const navItemDropDownList = document.querySelectorAll('.nav-item-dropdown');
+const navTogglerOpen = document.querySelector('.navbar-toggler-open');
+const navTogglerClose = document.querySelector('.navbar-toggler-close');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+
+
+
+navTogglerOpen.addEventListener('click', () =>{
+    navbarCollapseDiv.classList.add('show-navbar');
+});
+
+navTogglerClose.addEventListener('click', () => {
+    navbarCollapseDiv.classList.remove('show-navbar');
+});
+
+// animation and transition stopper
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
